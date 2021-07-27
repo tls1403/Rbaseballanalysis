548f509917a178b3efc8f1934f8fd6a06a9d96ca
tls1403 <tls1403@changwon.ac.kr>
2021-07-27 23:55:08 +0900

    2021-07-27 23:54 commit

--- 2021-07-26/2021-07-26.R
+++ 2021-07-26/2021-07-26.R
@@ -38,5 +38,74 @@ test <- subset(b,yearID<2016&yearID>1995)
 library(nnet)
 train$ID_a = class.ind(train$Divwin) #문자로 되어있는 Divwin을 0과 1로만 이뤄진 가변수로 만듬
 test$ID_b = class.ind(test$Divwin)
+View(train)
 
 fitnn = nnet(ID_a~ERA+AVG+X3R+RR,train, size=3,softmax = TRUE)
+
+fitnn
+
+summary(fitnn)
+
+
+c <- table(data.frame(predicted= predict(fitnn,test)[,2]>0.5,
+                      actual=test$ID_b[,2]>0.5))
+c
+
+(c[1,1]+c[2,2])/(c[1,1]+c[2,1]+c[1,2]+c[2,2])
+
+library(neuralnet)
+b$win=b$Divwin=='Y'
+b$not_win=b$Divwin=='N'
+View(b)
+d <- neuralnet(win+not_win~ERA+AVG+X3R+RR,b,hidden = 3,stepmax = 1e6)
+d$result.matrix
+plot(d)
+
+
+#모수선형분석
+library(MASS)
+ldafit <- lda(Divwin~ERA+AVG+X3R+RR,data = test)
+ldapred <- predict(ldafit)
+d <- table(ldapred$class,test$Divwin)
+(d[1,1]+d[2,2])/(d[1,1]+d[1,2]+d[2,1]+d[2,2])
+
+
+#모수비선형분석
+qdafit <- qda(Divwin~ERA+AVG+X3R+RR,data = test)
+qdapred <- predict(qdafit)
+e <- table(qdapred$class,test$Divwin)
+(e[1,1]+e[2,2])/(e[1,1]+e[1,2]+e[2,1]+e[2,2])
+
+
+
+
+team <- subset(Teams,yearID == 2015, select = c(teamID,X2B,X3B,ERA,RA))
+View(team)
+rownames(team) <- team[,1] #숫자로되어있던 인덱스 값들을 1열의 팀이름으로 바꿔줌
+team[,1] <- NULL # 그리고 팀1열의 값들은 NULL 처리
+team_st <- scale(team)#표준화
+
+install.packages("factoextra")
+library(factoextra)
+
+fviz_nbclust(team_st,kmeans,method = "gap_stat") #군집할 개수를 구함
+
+
+residual <- kmeans(team_st,3,nstart = 25)
+residual
+
+fviz_cluster(residual,data = team_st)
+
+
+library(FactoMineR)
+pca.res <- PCA(team_st, graph = FALSE)
+par(mfrow=c(1,1))
+fviz_contrib(pca.res,choice = "var",axes = 1,top = 4)
+fviz_contrib(pca.res,choice = "var",axes = 2,top = 4)
+
+x <- -5:5
+y <- x^2+x
+plot(x,y,type = "b")
+
+
+
